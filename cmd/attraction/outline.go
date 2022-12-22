8a00bb363cad9c56f903aff4971ee94cf31b39ca
Svetoslav Angelov <svetoslavangelov86@gmail.com>
2022-12-22 13:26:15 +0200

    Issue#2 Added tourist attraction data structures
    Created a new "attraction" folder and package, with a detail and outline tourist attraction data mode.

---
+++ cmd/attraction/outline.go
@@ -0,0 +1,36 @@
+package attraction
+
+type Outline struct {
+	/*
+		Tourist attraction outline data model.
+		This object is used to show the list
+		of tourist attractions near the user and
+		only key information is displayed in each
+		card presented to the user.
+	*/
+
+	// The ID can be a simple int, which can start from 1.
+	Id int32 `json:"id,omitempty"`
+
+	// The name of the tourist attraction, example "Cutty Sark".
+	Name string `json:"name"`
+
+	// Based on the HoursOpen & DaysOpen information from the
+	// Detail object, determine whether the attraction is open.
+	// This can be determined when the client requests this object.
+	IsOpen bool `json:"isOpen"`
+
+	// The distance between the client's current location and
+	// this tourist attraction. This can be determined when the
+	// client requests this object.
+	Distance string `json:"distance"`
+
+	// Apple Maps rating, if applicable, example "4.5".
+	Rating float32 `json:"rating"`
+
+	// 2D coordinate latitude, example "51.48286"
+	Latitude float64 `json:"latitude"`
+
+	// 2D coordinate longitutde, example "-0.00145"
+	Longitude float64 `json:"longitude"`
+}
