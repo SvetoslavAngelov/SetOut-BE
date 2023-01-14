af54e2a42a6fc10473bac818f70f232d8f1ec30a
Svetoslav Angelov <svetoslavangelov86@gmail.com>
2023-01-14 13:51:21 +0000

    Added a http request router
    Added:
    1. A http request router for the supported REST methods
    2. GetAttractionsById and GetAttractionsList methods
    3. Added a test store to test the http routes

--- go.mod
+++ go.mod
@@ -3,5 +3,5 @@ module github.com/SvetoslavAngelov/tourplan-app
 go 1.19
 
 require (
-	github.com/go-chi/chi/v5 v5.0.8 // indirect
+	github.com/go-chi/chi/v5 v5.0.8
 )
--- src/main.go
+++ src/main.go
@@ -1,46 +1,19 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
-	"strconv"
 
-	"github.com/SvetoslavAngelov/tourplan-app/src/attraction"
+	"github.com/SvetoslavAngelov/tourplan-app/src/routes"
 	"github.com/go-chi/chi/v5"
 )
 
-var attractions = []attraction.Outline{
-	{Id: 1,
-		Name:      "MyfirstAttr",
-		IsOpen:    false,
-		Distance:  2.64,
-		Rating:    4.7,
-		Latitude:  -0.000536,
-		Longitude: 51.476833,
-		ImageName: "default"},
-}
-
-func getAttraction(wr http.ResponseWriter, req *http.Request) {
-	wr.Header().Set("Content-Type", "application/json")
-	id, _ := strconv.Atoi(chi.URLParam(req, "id"))
-	for _, attr := range attractions {
-
-		// Casting down to int32, unlikely to max it out.
-		if attr.Id == int32(id) {
-			json.NewEncoder(wr).Encode(attr)
-			return
-		}
-
-	}
-
-	wr.WriteHeader(http.StatusNotFound)
-	json.NewEncoder(wr).Encode("{}")
-
-}
-
 func main() {
 	router := chi.NewRouter()
-	router.Get("/attraction/{id:[0-9]+}", getAttraction)
+
+	router.Route("/", func(r chi.Router) {
+		r.Get("/attractions/{id:[0-9]+}", routes.GetAttractionById)
+		r.Get("/attractions", routes.GetAttractionsList)
+	})
 
 	err := http.ListenAndServe(":8080", router)
 	if err != nil {
